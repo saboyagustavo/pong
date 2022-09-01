4052ae66aa2b877658b989bf0139228191b6d195
Gustavo Saboya <saboya.gustavo@gmail.com>
2022-09-01 00:16:10 -0300

    Animating the ball: added movements.

--- index.js
+++ index.js
@@ -1,8 +1,67 @@
+class Vector {
+  constructor(x = 0, y = 0) {
+    this.x = x;
+    this.y = y;
+  }
+}
+
+class Rectangle {
+  constructor(width, height) {
+    this.position = new Vector;
+    this.size = new Vector(width, height);
+  }
+}
+
+class Ball extends Rectangle {
+  constructor() {
+    super(10, 10);
+    this.velocity = new Vector;
+  }
+}
+
 const canvas = document.getElementById('pong');
 const context = canvas.getContext('2d');
 
-context.fillStyle = '#000000';
-context.fillRect(0, 0, canvas.width, canvas.height);
+const ball = new Ball;
+ball.velocity.x = 100;
+ball.velocity.y = 100;
 
-context.fillStyle = '#ffffff';
-context.fillRect(0, 0, 10, 10);
+
+// calculate how much time has elapsed since the last requestAnimationFrame
+let lastTime;
+function frameCallback(milliSeconds) {
+  if (lastTime) {
+    const difference = milliSeconds - lastTime;
+    update(difference / 1000);
+  }
+
+  lastTime = milliSeconds;
+
+  //requestAnimationFrame is a function that takes a callback
+  // then calls callback for the next time the browser is ready to draw
+  // in the callback you'll get the elapsed time since the page was loaded
+  requestAnimationFrame(frameCallback);
+}
+
+function update(deltaTiming) {
+  ball.position.x += ball.velocity.x * deltaTiming;
+  ball.position.y += ball.velocity.y * deltaTiming;
+
+  context.fillStyle = '#000000';
+  context.fillRect(
+    0,
+    0,
+    canvas.width,
+    canvas.height
+  );
+
+  context.fillStyle = '#ffffff';
+  context.fillRect(
+    ball.position.x,
+    ball.position.y,
+    ball.size.x,
+    ball.size.y
+  );
+}
+
+frameCallback();
